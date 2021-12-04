c1ca250b4782698eb502537d1cd65884091c4652
Adi Shohat <adi.shohat21@gmail.com>
2021-12-04 14:47:45 +0200

    add chat credentials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
+import { ConsoleSqlOutlined } from '@ant-design/icons';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <ChatEngine 
+      height="100vh"
+      projectID={process.env.REACT_APP_CHAT_PROJECT_ID}
+      userName={process.env.REACT_APP_CHAT_USERNAME}
+      userSecret={process.env.REACT_APP_CHAT_USER_SECRET}
+    />
   );
 }
 
